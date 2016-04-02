ed4bc8410bec7c55216037938dd251069189edd1
hoanghunglhu <hoanghunglhu@gmail.com>
2016-04-02 11:04:21 +0700

    Fix css and js of view

--- application/controllers/HtmlController.php
+++ application/controllers/HtmlController.php
@@ -4,23 +4,25 @@
         public function __construct(){
             
             parent::__construct();
+            $this->load->helper("url");
             $this->load->library("domparser");
             
         }
         
         public function index(){
 
-            for($i = 1;$i <= 1; $i++){
-                $website = $this->domparser->file_get_html("https://tinhte.vn/?wpage=".$i);
-                foreach($website->find("a.newsTitle") as $key){
-                    $link = "https://tinhte.vn/".$key->href;
-                    //echo $link;
-                    $page = $this->domparser->file_get_html($link);
-                    $title = $page->find("p.title",0);
-                    $content = $page->find("article", 0)->plaintext;
-                    echo '<span style="color:red">'.$title.'</span><br/>'.$content.'<br/>';
-                }
-            }
+//            for($i = 1;$i <= 1; $i++){
+//                $website = $this->domparser->file_get_html("https://tinhte.vn/?wpage=".$i);
+//                foreach($website->find("a.newsTitle") as $key){
+//                    $link = "https://tinhte.vn/".$key->href;
+//                    //echo $link;
+//                    $page = $this->domparser->file_get_html($link);
+//                    $title = $page->find("p.title",0);
+//                    $content = $page->find("article", 0)->plaintext;
+//                    echo '<span style="color:red">'.$title.'</span><br/>'.$content.'<br/>';
+//                }
+//            }
+            $this->load->view("index.php");
         }
     }
 ?>
--- application/views/index.php
+++ application/views/index.php
@@ -1,65 +1,255 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <!-- Meta, title, CSS, favicons, etc. -->
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Fox Design</title>
-  <!-- Bootstrap core CSS -->
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <link href="fonts/css/font-awesome.min.css" rel="stylesheet">
-  <link href="css/animate.min.css" rel="stylesheet">
-  <!-- Custom styling plus plugins -->
-  <link href="css/custom.css" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="css/maps/jquery-jvectormap-2.0.3.css" />
-  <link href="css/icheck/flat/green.css" rel="stylesheet" />
-  <link href="css/floatexamples.css" rel="stylesheet" type="text/css" />
-  <script src="js/jquery.min.js"></script>
-  <script src="js/nprogress.js"></script>
-  <script src="menu/js.html"></script>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Fox Design</title>
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url("assets/css/bootstrap.min.css "); ?>" rel="stylesheet">
+    <link href="<?php echo base_url("assets/fonts/css/font-awesome.min.css "); ?>" rel="stylesheet">
+    <link href="<?php echo base_url("assets/css/animate.min.css "); ?>" rel="stylesheet">
+    <!-- Custom styling plus plugins -->
+    <link href="<?php echo base_url("assets/css/custom.css "); ?>" rel="stylesheet">
+    <link href="<?php echo base_url("assets/css/maps/jquery-jvectormap-2.0.3.css "); ?>" rel="stylesheet" type="text/css" />
+    <link href="<?php echo base_url("assets/css/icheck/flat/green.css "); ?>" rel="stylesheet" />
+    <link href="<?php echo base_url("assets/css/floatexamples.css "); ?>" rel="stylesheet" type="text/css" />
+    <script src="<?php echo base_url("assets/js/jquery.min.js "); ?>"></script>
+    <script src="<?php echo base_url("assets/js/nprogress.js "); ?>"></script>
+    <script src="<?php echo base_url("assets/menu/js.html "); ?>"></script>
 </head>
+
 <body class="nav-md">
-<form  method="POST" action="index.php" >
-	<div class="container body">
-		<div class="main_container">
-			<div class="col-md-3 left_col">
-				<div class="left_col scroll-view">
-					<div class="navbar nav_title" style="border: 0;">
-						<a href="index.php" class="site_title"><i class="fa fa-paw"></i> <span>Fox Design</span></a>
-					</div>
-					<div class="clearfix"></div>
-					<?php require_once 'menu/menuprile.html';
-					 	  require_once 'menu/siderbar.html';
-					 	  require_once 'menu/menuFooter.html';
-					?>
-				</div>
-			</div>
-			<?php require_once 'menu/topnav.html';?>
-			<!-- page content -->
-			<div class="right_col" role="main">
-				<div class="panel">
-					<?php require_once 'xuly/getContent.php';?>
-				</div>
-				<!-- /page content -->
-				<!-- footer content -->
-				<?php require_once 'menu/footer.html';?>
-				<!-- /footer content -->
-			</div>
-			<!-- /page content -->
-		</div>
-	</div>
-	<div id="custom_notifications" class="custom-notifications dsp_none">
-		<ul class="list-unstyled notifications clearfix" data-tabbed_notifications="notif-group">
-		</ul>
-		<div class="clearfix"></div>
-		<div id="notif-group" class="tabbed_notifications"></div>
-	</div>
-	<!--javascrip-->
-	<?php require_once 'menu/js.html';?>
-	<!-- /footer content -->
-	</form>
+    <div class="container body">
+        <div class="main_container">
+            <div class="col-md-3 left_col">
+                <div class="left_col scroll-view">
+                    <div class="navbar nav_title" style="border: 0;">
+                        <a href="index.php" class="site_title"><i class="fa fa-paw"></i> <span>Fox Design</span></a>
+                    </div>
+                    <div class="clearfix"></div>
+                    <!-- menu prile quick info -->
+                    <div class="profile">
+                        <div class="profile_pic">
+                            <img src="images/img.jpg" alt="..." class="img-circle profile_img">
+                        </div>
+                        <div class="profile_info">
+                            <span>Chào,</span>
+                            <h2>Hana Tiên</h2>
+                        </div>
+                    </div>
+                    <!-- sidebar menu -->
+                    <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+                        <div class="menu_section">
+                            <h3>Cài đặt chung</h3>
+                            <ul class="nav side-menu">
+                                <li><a><i class="fa fa-home"></i> Trang Chủ <span class="fa fa-chevron-down"></span></a>
+                                    <ul class="nav child_menu" style="display: none">
+                                        <li><a href="thongtinthanhvien.php">Thông Tin Thành Viên</a>
+                                        </li>
+                                        <li><a href="giaovien.php">Giáo Viên Hướng Dẫn</a>
+                                        </li>
+                                        <li><a href="tailieu.php">Tài Liệu Tham Khảo</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- /menu footer buttons -->
+                    <div class="sidebar-footer hidden-small">
+                        <a data-toggle="tooltip" data-placement="top" title="Settings">
+                            <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+                        </a>
+                        <a data-toggle="tooltip" data-placement="top" title="FullScreen">
+                            <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
+                        </a>
+                        <a data-toggle="tooltip" data-placement="top" title="Lock">
+                            <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
+                        </a>
+                        <a data-toggle="tooltip" data-placement="top" title="Logout">
+                            <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
+                        </a>
+                    </div>
+                    <!-- /menu footer buttons -->
+                    <!-- /sidebar menu -->
+                    <!-- /menu prile quick info -->
+                </div>
+            </div>
+            <!-- top navigation -->
+            <div class="top_nav">
+                <div class="nav_menu">
+                    <nav class="" role="navigation">
+                        <div class="nav toggle">
+                            <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+                        </div>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="">
+                                <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                                    <img src="images/img.jpg" alt="">Hana Tiên
+                                    <span class=" fa fa-angle-down"></span>
+                                </a>
+                                <ul class="dropdown-menu dropdown-usermenu animated fadeInDown pull-right">
+                                    <li><a href="javascript:;">  Profile</a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:;">
+                                            <span class="badge bg-red pull-right">50%</span>
+                                            <span>Settings</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:;">Help</a>
+                                    </li>
+                                    <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <!-- /top navigation -->
+            <!-- page content -->
+            <div class="right_col" role="main">
+                <div class="panel">
+                    <h3 class="text-center">Vui Lòng Nhập Thông Tin</h3>
+                    <div class="row">
+                        <div class="col-md-10 col-md-offset-1 form-group">
+                            <div class="input-group input-group-lg panelbox">
+                                <input type="text" name="keyword" class="form-control" placeholder="Nhập từ khóa cần tìm..." aria-describedby="sizing-addon1">
+                                <span class="input-group-addon" id="sizing-addon1">?</span>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.row -->
+                    <div class="row">
+                        <div class="col-md-10 col-md-offset-1 form-group">
+                            <div class="input-group input-group-lg panelbox">
+                                <input type="text" name="txtUrl" class="form-control" placeholder="Nhập trang web cần tìm..." aria-describedby="sizing-addon1">
+                                <span class="input-group-addon" id="sizing-addon1">?</span>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.row -->
+                    <div class="row">
+                        <div class="col-md-10 col-md-offset-1 form-group">
+                            <div class="input-group input-group-lg panelbox">
+                                <input type="text" name="txtTag" class="form-control" placeholder="Nhập tên tag chứa bài viết..." aria-describedby="sizing-addon1">
+                                <span class="input-group-addon" id="sizing-addon1">?</span>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.row -->
+                    <div class="row">
+                        <div class="col-md-10 col-md-offset-1 form-group">
+                            <div class="input-group input-group-lg panelbox">
+                                <input type="text" name="txtTitle" class="form-control" placeholder="Nhập tên tag chứa tiêu đề..." aria-describedby="sizing-addon1">
+                                <span class="input-group-addon" id="sizing-addon1">?</span>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.row -->
+                    <div class="row">
+                        <div class="col-md-10 col-md-offset-1 form-group">
+                            <div class="input-group input-group-lg panelbox">
+                                <input type="text" name="txtContent" class="form-control" placeholder="Nhập tên tag chứa nội dung chính bài viết..." aria-describedby="sizing-addon1">
+                                <span class="input-group-addon" id="sizing-addon1">?</span>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.row -->
+                    <div class="row">
+                        <div class="col-md-10 col-md-offset-1 form-group">
+                            <div class="input-group input-group-lg panelbox">
+                                <input type="text" name="txtFolder" class="form-control" placeholder="Nhập tập tin để lưu file..." aria-describedby="sizing-addon1">
+                                <span class="input-group-addon" id="sizing-addon1">?</span>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.row -->
+                    <div class="row">
+                        <div class="col-md-2 col-md-offset-4 form-group">
+                            <span class="pagebtny">	
+                                            <button type="submit" name="submit" class="btn" >Thực Hiện</button></span>
+                        </div>
+                        <div class="col-md-2 form-group">
+                            <span class="pagebtnn">	
+                                            <button type="button" class="btn">Hủy Bỏ</button></span>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+                    <!-- /.row -->
+                    <div class="row">
+                        <div id="resultContent" class="col-md-12 form-group">
+
+                        </div>
+                    </div>
+                    <!-- /.row -->
+
+                </div>
+                <!-- /page content -->
+                <!-- footer content -->
+                <footer>
+                    <div class="copyright-info">
+                        <p class="pull-right">@Copyright by <a href="https://facebook.com/camtien1503">Hana Tiên</a>
+                        </p>
+                    </div>
+                    <div class="clearfix"></div>
+                </footer>
+                <!-- /footer content -->
+            </div>
+            <!-- /page content -->
+        </div>
+    </div>
+    <div id="custom_notifications" class="custom-notifications dsp_none">
+        <ul class="list-unstyled notifications clearfix" data-tabbed_notifications="notif-group">
+        </ul>
+        <div class="clearfix"></div>
+        <div id="notif-group" class="tabbed_notifications"></div>
+    </div>
+    <!--javascrip-->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- gauge js -->
+    <script type="text/javascript" src="<?php echo base_url("assets/js/gauge/gauge.min.js "); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url("assets/js/gauge/gauge_demo.js "); ?>"></script>
+    <!-- bootstrap progress js -->
+    <script type="text/javascript" src="<?php echo base_url("assets/js/progressbar/bootstrap-progressbar.min.js "); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url("assets/js/nicescroll/jquery.nicescroll.min.js "); ?>"></script>
+    <!-- icheck -->
+    <script type="text/javascript" src="<?php echo base_url("assets/js/icheck/icheck.min.js "); ?>"></script>
+    <!-- daterangepicker -->
+    <script type="text/javascript" src="<?php echo base_url("assets/js/moment/moment.min.js "); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url("assets/js/datepicker/daterangepicker.js "); ?>"></script>
+    <!-- chart js -->
+    <script type="text/javascript" src="<?php echo base_url("assets/js/chartjs/chart.min.js "); ?>"></script>
+
+    <script type="text/javascript" src="<?php echo base_url("assets/js/custom.js "); ?>"></script>
+
+    <!-- flot js -->
+    <!--[if lte IE 8]><script type="text/javascript" src="js/excanvas.min.js"></script><![endif]-->
+    <script type="text/javascript" src="<?php echo base_url("assets/js/flot/jquery.flot.js "); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url("assets/js/flot/jquery.flot.pie.js "); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url("assets/js/flot/jquery.flot.orderBars.js "); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url("assets/js/flot/jquery.flot.time.min.js "); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url("assets/js/flot/date.js "); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url("assets/js/flot/jquery.flot.spline.js "); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url("assets/js/flot/jquery.flot.stack.js "); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url("assets/js/flot/curvedLines.js "); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url("assets/js/flot/jquery.flot.resize.js "); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url("assets/js/maps/jquery-jvectormap-2.0.3.min.js "); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url("assets/js/maps/gdp-data.js "); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url("assets/js/maps/jquery-jvectormap-world-mill-en.js "); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url("assets/js/maps/jquery-jvectormap-us-aea-en.js "); ?>"></script>
+    <!-- pace -->
+    <script type="text/javascript" src="<?php echo base_url("assets/js/pace/pace.min.js "); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url("assets/js/skycons/skycons.min.js "); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url("assets/js/myJS.js "); ?>"></script>
+    <!-- /footer content -->
 </body>
 
 </html>
